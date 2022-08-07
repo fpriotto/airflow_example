bc30897ef747d9171d7ba4d52a21f39dad8f264a
Felipe Priotto <felipe_m_priotto@hotmail.com>
2022-08-07 13:24:57 -0300

    fixed scheduler path to airflow_home

--- run-server.sh
+++ run-server.sh
@@ -2,4 +2,5 @@
 # script to run airflow webserver and scheduler in background using a virtual environment
 
 source .env/bin/activate
+export AIRFLOW_HOME=$(pwd)/.airflow_home
 airflow webserver -p 8080 & airflow scheduler
